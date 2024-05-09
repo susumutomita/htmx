e45d7554a50a4fe746eef72dbfa2cdb3f5e8ecd1
susumu tomita <oyster880@gmail.com>
2024-05-10 08:59:47 +0900

    use daisui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,31 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
+
   daisyui: {
-    themes: [
-      {
-        mytheme: {
 
-          "primary": "#ff00ff",
+    themes: ["light", "dark", "cupcake"],
 
-          "secondary": "#ff00ff",
-
-          "accent": "#00ffff",
-
-          "neutral": "#ff00ff",
-
-          "base-100": "#ff00ff",
-
-          "info": "#0000ff",
-
-          "success": "#00ff00",
-
-          "warning": "#00ff00",
-
-          "error": "#ff0000",
-        },
-      },
-    ],
   },
   plugins: [
     require('daisyui'),
